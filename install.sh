f7a9efcd6ab0f3c0a1cf1f13cd9c02b186d41196
SiimonnLeGalerien <morel.simon77@gmail.com>
2025-09-30 21:28:41 +0200

    cleaner install process

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-javac *.java
+mkdir -p build
+javac *.java -d build
+cd build
 jar cfe chore.jar Main *.class
 mkdir -p ~/.local/lib/chore
 mv chore.jar ~/.local/lib/chore/
+cd ..
 cp chore ~/.local/bin/
 cp chores.csv ~/.local/lib/chore/chores.csv
 echo "Chore installed successfuly"
